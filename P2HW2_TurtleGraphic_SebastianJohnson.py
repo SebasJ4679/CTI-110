c72dac79255aef2b32ebfcafe65172c0052b2fc7
SebasJ4679 <55717007+SebasJ4679@users.noreply.github.com>
2019-09-23 16:40:00 -0400

    Add files via upload
    Second Submission

---
+++ P2HW2_TurtleGraphic_SebastianJohnson.py
@@ -0,0 +1,37 @@
+# In this project i will practicing how to use libraries and draw figures on a canvas
+# 9/22/2019
+# CTI-110 P2HW2 - Turtle Graphic
+# Sebastian Johnson
+# 1. Import the turle
+# 2. decide what shape you plan to make
+# 3. play around with angles for hours until you figure out how to make the shape you're trying to make
+
+
+
+import turtle
+
+
+
+turtle.speed(2)
+turtle.left(45)
+turtle.forward(60)
+turtle.left(90)
+turtle.forward(60)
+turtle.left(90)
+turtle.forward(60)
+turtle.left(90)
+turtle.forward(60)
+turtle.penup()
+turtle.left(90)
+turtle.forward(60)
+turtle.pendown()
+turtle.forward(60)
+turtle.right(90)
+turtle.forward(60)
+turtle.right(90)
+turtle.forward(60)
+turtle.right(90)
+turtle.forward(60)
+turtle.hideturtle()
+
+
