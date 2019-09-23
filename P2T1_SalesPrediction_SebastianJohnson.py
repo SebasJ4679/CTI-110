cce01a919a253f84ea180ab7c577434c0ac6de9e
SebasJ4679 <55717007+SebasJ4679@users.noreply.github.com>
2019-09-23 16:39:35 -0400

    Add files via upload
    First submission

---
+++ P2T1_SalesPrediction_SebastianJohnson.py
@@ -0,0 +1,15 @@
+# Get the projected total sales.
+# 9/22/2019
+# CTI-110 P2T1 - Sales Prediction
+# Sebastian Johnson
+#
+
+
+
+total_sales = float(input('Enter the projected sales: '))
+
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit
+print('The profit is $', format(profit, ',.2f'))
